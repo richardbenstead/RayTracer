65163a5d9a895187882c4d754ba03a7fc4817893
Richard Benstead <rbenstead@gmail.com>
2023-01-14 00:39:09 +1100

    wip

--- src/raytrace.h
+++ src/raytrace.h
@@ -2,12 +2,16 @@
 #include <cstdlib>
 #include <cstdio>
 #include <fstream>
-#include "geometry.h"
+// #include "geometry.h"
 #include <cmath>
 #include <vector>
 #include <limits>
 #include <memory>
 #include "shapes.h"
+#include <Eigen/Dense>
+
+using Vec3d = Eigen::Vector3d;
+using Matrix43d = Eigen::Matrix<double,4,3>;
 
 //TODO: Add wavelet noise textures
 // torus analytical intersect
@@ -20,10 +24,10 @@
 class PointLight
 {
 public:
-    PointLight(const Vec3f& p, const Vec3f& c, const float& i) : pos(p), col(c), intensity(i) {}
-    Vec3f pos;
-    Vec3f col;
-    float intensity;
+    PointLight(const Vec3d& p, const Vec3d& c, const double& i) : pos(p), col(c), intensity(i) {}
+    Vec3d pos;
+    Vec3d col;
+    double intensity;
 };
 
 class Scene
@@ -31,70 +35,55 @@ class Scene
 public:
     Scene()
     {
-        mObjects.push_back(std::make_shared<Plane<TextureCheck>>(Vec3f(0, 1, 0), Vec3f(0, -2, 0)));
-        // mObjects.push_back(std::make_shared<Cube>(Vec3f(1.5)));
-        // mObjects.push_back(std::make_shared<Torus>(Vec3f(2, 0.5, 0.0), 2, 0.65));
-        mObjects.push_back(std::make_shared<Sphere<TextureCheck>>(Vec3f(-3, 0.0, 0.0), 2));
-        mObjects.push_back(std::make_shared<Sphere<TextureCheck>>(Vec3f(3, 0.0, 0.0), 1));
+        mObjects.push_back(std::make_shared<Plane<TextureCheck>>(Vec3d(0, 1, 0), Vec3d(0, -2, 0)));
+        // mObjects.push_back(std::make_shared<Cube>(Vec3d(1.5)));
+        // mObjects.push_back(std::make_shared<Torus>(Vec3d(2, 0.5, 0.0), 2, 0.65));
+        mObjects.push_back(std::make_shared<Sphere<TextureCheck>>(Vec3d(-3, 0.0, 0.0), 2));
+        mObjects.push_back(std::make_shared<Sphere<TextureCheck>>(Vec3d(3, 0.0, 0.0), 1));
         /*
         mObjects.push_back(std::make_shared<Blend>(
-            std::make_shared<Cube>(Vec3f(1.5)),
+            std::make_shared<Cube>(Vec3d(1.5)),
             std::make_shared<Torus>(2, 0.65), 5));
         mObjects.push_back(std::make_shared<Mix<Cube, Sphere>>(
-            Cube(Vec3f(1)),
-            Sphere(Vec3f(0), 1), 0.5));
+            Cube(Vec3d(1)),
+            Sphere(Vec3d(0), 1), 0.5));
             */
         //mObjects.push_back(std::make_shared<SoftObject>());
 
-        mLights.push_back(std::make_unique<PointLight>(Vec3f( 20, 10,  20), Vec3f(1.0, 0.2, 0.2), 10000));
-        mLights.push_back(std::make_unique<PointLight>(Vec3f( 0, 10, 20), Vec3f(0.2, 1, 0.2), 5000));
-        mLights.push_back(std::make_unique<PointLight>(Vec3f( -20, 10,  20), Vec3f(0.2, 0.2, 1.0), 10000));
-        mLights.push_back(std::make_unique<PointLight>(Vec3f( 0, 10,  -20), Vec3f(1, 1, 1), 10000));
+        mLights.push_back(std::make_unique<PointLight>(Vec3d( 20, 10,  20), Vec3d(1.0, 0.2, 0.2), 10000));
+        mLights.push_back(std::make_unique<PointLight>(Vec3d( 0, 10, 20), Vec3d(0.2, 1, 0.2), 5000));
+        mLights.push_back(std::make_unique<PointLight>(Vec3d( -20, 10,  20), Vec3d(0.2, 0.2, 1.0), 10000));
+        mLights.push_back(std::make_unique<PointLight>(Vec3d( 0, 10,  -20), Vec3d(1, 1, 1), 10000));
 
-        lookAt(Vec3f(0, 1, 9), 0);
+        lookAt(Vec3d(0, 1, 9), Vec3d(0,0,0));
     };
 
-    void update(const float time)
+    void update(const double time)
     {
-        lookAt(Vec3f(0, 1, 2+time/10.0f), 0);
+        lookAt(Vec3d(0, 1, 2+time/10.0f), Vec3d(0,0,0));
     }
 
     std::vector<std::shared_ptr<ImplicitShape>> mObjects;
     std::vector<std::unique_ptr<PointLight>> mLights;
+
     Matrix44f mCamToWorld; // normalized vectors for x,y,z orientation
 
-    void setCameraOrientation(const Vec3f& z, Vec3f camUp = Vec3f(0, 1, 0))
+    void setCameraOrientation(const Vec3d& z, Vec3d camUp = Vec3d(0, 1, 0))
     {
-        Vec3f x = camUp.normalize().crossProduct(z).normalize();
-        Vec3f y = z.crossProduct(x);
-
-        // set x-axis
-        mCamToWorld[0][0] = x[0];
-        mCamToWorld[0][1] = x[1];
-        mCamToWorld[0][2] = x[2];
-
-        // set y-axis
-        mCamToWorld[1][0] = y[0];
-        mCamToWorld[1][1] = y[1];
-        mCamToWorld[1][2] = y[2];
-
-        // set z-axis
-        mCamToWorld[2][0] = z[0];
-        mCamToWorld[2][1] = z[1];
-        mCamToWorld[2][2] = z[2];
+        mCamToWorld.row(0) = (camUp.crossProduct(z)).normalize();
+        mCamToWorld.row(1) = z.crossProduct(x);
+        mCamToWorld.row(2) = z;
     }
 
 private:
-    void lookAt(const Vec3f& camPos, const Vec3f& to, Vec3f camUp = Vec3f(0, 1, 0))
+    void lookAt(const Vec3d& camPos, const Vec3d& to, Vec3d camUp = Vec3d(0, 1, 0))
     {
         // because the forward axis in a right hand coordinate system points backward we compute -(to - camPos)
-        Vec3f z = (camPos - to).normalize();  // vector in direction that camera points
+        Vec3d z = (camPos - to).normalized(); // vector in direction that camera points
         setCameraOrientation(z, camUp);
 
         // set position
-        mCamToWorld[3][0] = camPos[0];
-        mCamToWorld[3][1] = camPos[1];
-        mCamToWorld[3][2] = camPos[2];
+        mCamToWorld.row(3) = camPos[0];
     }
 };
 
@@ -102,12 +91,12 @@ private:
 class RayTrace
 {
 public:
-    float traceShadow(const Vec3f& rayOrigin,
-                           const Vec3f& rayDirection, 
+    float traceShadow(const Vec3d& rayOrigin,
+                           const Vec3d& rayDirection, 
                            const float& maxDistance,
                            const ImplicitShape* exclude)
     { 
-        float distance;
+        double distance;
         for (const auto& shape : mScene.mObjects) {
             if (shape.get() != exclude) {
                 [[likely]];
@@ -121,14 +110,14 @@ public:
         return 0.0f; 
     }
 
-    Vec3f traceRay(const Vec3f& rayOrigin, const Vec3f& rayDirection)
+    Vec3d traceRay(const Vec3d& rayOrigin, const Vec3d& rayDirection)
     {
         ImplicitShape* isectShape = nullptr;
-        float minDistance = std::numeric_limits<float>::max();
+        double minDistance = std::numeric_limits<double>::max();
 
         // check all shapes for first intersection
         for (const auto& shape : mScene.mObjects) {
-            float distance;
+            double distance;
             if (shape->intersect(rayOrigin, rayDirection, distance) &&
                (distance < minDistance))
             {
@@ -137,20 +126,21 @@ public:
                 minDistance = distance;
             }
         }
-        if (minDistance != std::numeric_limits<float>::max())
+        if (minDistance != std::numeric_limits<double>::max())
         {
             [[unlikely]];
-            Vec3f surfaceNorm;
-            Vec3f color;
-            Vec3f isectPoint = rayOrigin + minDistance * rayDirection;
+            Vec3d surfaceNorm;
+            Vec3d color;
+            Vec3d isectPoint = rayOrigin + minDistance * rayDirection;
             isectShape->getSurfaceData(isectPoint, surfaceNorm, color);
          
             // loop over all lights in the scene and add their contribution to P's brightness
-            Vec3f R{0.5, 0.5, 0.5};
+            Vec3d R{0.5, 0.5, 0.5};
             constexpr float decayScale = 8.0f;
             for (const auto& light: mScene.mLights) {
-                Vec3f lightDir = light->pos - isectPoint;
-                const float dist = lightDir.makeNormed(); 
+                Vec3d lightDir = light->pos - isectPoint;
+                const float dist = lightDir.norm(); 
+		lightDir /= dist;
 
                 // Lambertian shading. The light per unit area depends on incidence angle vs normal
                 const float cosAngle = lightDir.dotProduct(surfaceNorm);
@@ -165,27 +155,35 @@ public:
 
                 // diffuse light is independent of light incident angle
             }
-            return R * color / (decayScale * minDistance);
+            return R.cwiseProduct(color) / (decayScale * minDistance);
         }
 
-        return 0; 
+        return Vec3d(0,0,0);
     }
 
     void draw(auto& image)
     {
         //mScene.update(mTime++);
         constexpr int16_t width = image.GRID_SIZE, height = image.GRID_SIZE;
-        constexpr float ratio = width / static_cast<float>(height);
-        float angle = tan((mFov * 0.5) / 180.f * M_PI);
+        constexpr double ratio = width / static_cast<double>(height);
+        double angle = tan((mFov * 0.5) / 180.f * M_PI);
 
-        Vec3f rayOrigin = mScene.mCamToWorld.multVecMatrix(Vec3f(0));
+        Vec3d src{0,0,0};
+        Eigen::Vector4d tmp = mScene.mCamToWorld * src;
+        Vec3d rayOrigin{tmp[0]/tmp[3], tmp[1]/tmp[3], tmp[2]/tmp[3]};
+
+        // Vec3d rayOrigin = mScene.mCamToWorld.multVecMatrix(Vec3d(0));
         for (uint32_t j = 0; j < height; ++j) {
             for (uint32_t i = 0; i < width; ++i) {
-                float x = (2 * i / static_cast<float>(width) - 1) * ratio * angle;
-                float y = (1 - j / static_cast<float>(height) * 2) * angle;
-                Vec3f rayDirection = mScene.mCamToWorld.multDirMatrix(Vec3f(x, y, -1).normalize());
-                Vec3f pixelColor = traceRay(rayOrigin, rayDirection);
-                image.image[image.POS(i, j)] = Pixel{std::min(1.0f, pixelColor[0]), std::min(1.0f, pixelColor[1]), std::min(1.0f, pixelColor[2])};
+                double x = (2 * i / static_cast<double>(width) - 1) * ratio * angle;
+                double y = (1 - j / static_cast<double>(height) * 2) * angle;
+                // Vec3d rayDirection = mScene.mCamToWorld.multDirMatrix(Vec3d(x, y, -1).normalized());
+                Vec3d rayDirection = (mScene.mCamToWorld * (Vec3d(x, y, -1).normalized())).head(3);
+
+                Vec3d pixelColor = sphereTrace(rayOrigin, rayDirection);
+                image.image[image.POS(i, j)] = Pixel{std::min<float>(1.0, pixelColor[0]),
+                                                    std::min<float>(1.0, pixelColor[1]),
+                                                    std::min<float>(1.0, pixelColor[2])};
             }
         }
     }
@@ -196,7 +194,7 @@ public:
         // x move on the screen rotates the camera z to the right
         const float fovRad = mFov * M_PI / 180.0f;
         // set z-axis
-        Vec3f z{mScene.mCamToWorld[2][0],
+        Vec3d z{mScene.mCamToWorld[2][0],
                 mScene.mCamToWorld[2][1],
                 mScene.mCamToWorld[2][2]};
         const float cosX = cos(moveX * fovRad);
--- src/shapes.h
+++ src/shapes.h
@@ -2,12 +2,15 @@
 #include <iostream>
 #include <vector>
 #include "utils.h"
+#include <Eigen/Dense>
 
-static constexpr float INTERSECT_TH = 1e-2;
-static constexpr float MAX_DISTANCE = 50;
+static constexpr double INTERSECT_TH = 1e-2;
+static constexpr double MAX_DISTANCE = 50;
+
+using Vec3d = Eigen::Vector3d;
 
 struct TextureCheck {
-  float getVal(const float x, const float y, const float scale = 20) const {
+  double getVal(const double x, const double y, const double scale = 20) const {
     return ((int8_t(x * scale) ^ int8_t(y * scale)) & 1);
   }
 };
@@ -18,104 +21,101 @@ public:
   virtual ~ImplicitShape() {}
 
   // default intersection using sphere-trace
-  virtual bool intersect(const Vec3f &orig, const Vec3f &dir,
-                         float &t) const = 0;
+  virtual bool intersect(const Vec3d &orig, const Vec3d &dir, double &t) const = 0;
 
-  // Compute the surface data such as normal and texture coordinates at the
-  // intersection point.
-  virtual void getSurfaceData([[maybe_unused]] const Vec3f &point, Vec3f &norm,
-                              Vec3f &col) const = 0;
+  // Compute the surface data such as normal and texture coordinates at the intersection point.
+  virtual void getSurfaceData([[maybe_unused]] const Vec3d &point, Vec3d &norm,
+                              Vec3d &col) const = 0;
 
-  Vec3f color{1, 1, 1};
+  Vec3d color{1, 1, 1};
 };
 
 // Implicit sphere surface
 template <typename TEX_T = TextureCheck> class Sphere : public ImplicitShape {
 public:
-  Sphere(const Vec3f &c, const float &r)
+  Sphere(const Vec3d &c, const double &r)
       : center{c}, radius{r}, radius2{r * r} {}
 
-  bool intersect(const Vec3f &orig, const Vec3f &dir, float &t) const {
-    const Vec3f L = center - orig;
-    const float tca = L.dotProduct(dir); // project L along dir to get distance
+  bool intersect(const Vec3d &orig, const Vec3d &dir, double &t) const {
+    const Vec3d L = center - orig;
+    const double tca = L.dotProduct(dir); // project L along dir to get distance
                                          // to point tangental to center
-    const float d2 =
+    const double d2 =
         L.norm() -
         tca * tca; // square of distance from tangent to center via pythagoras
     if ((d2 > radius2) || (tca < 0)) {
       [[likely]];
       return false;
     }
-    const float thc = sqrt(radius2 - d2);
+    const double thc = sqrt(radius2 - d2);
     t = tca - thc; // first intersect
     return true;
   }
 
-  void getSurfaceData([[maybe_unused]] const Vec3f &point, Vec3f &norm,
-                      Vec3f &col) const {
+  void getSurfaceData([[maybe_unused]] const Vec3d &point, Vec3d &norm,
+                      Vec3d &col) const {
     norm = (point - center).normalize();
     col = color * texture.getVal((1 + atan2(norm.z, norm.x) / M_PI) * 0.5,
                                  acosf(norm.y) / M_PI);
   }
   const TEX_T texture{};
-  const Vec3f center;
-  const float radius, radius2;
+  const Vec3d center;
+  const double radius, radius2;
 };
 
 // Implicit plane surface
 template <typename TEX_T = TextureCheck> class Plane : public ImplicitShape {
 public:
-  Plane(const Vec3f &_normal = Vec3f(0, 1, 0), const Vec3f &pp = Vec3f(0))
+  Plane(const Vec3d &_normal = Vec3d(0, 1, 0), const Vec3d &pp = Vec3d(0))
       : normal(_normal), pointOnPlane(pp) {}
 
-  bool intersect(const Vec3f &orig, const Vec3f &dir, float &t) const {
+  bool intersect(const Vec3d &orig, const Vec3d &dir, double &t) const {
     // assuming vectors are all normalized
     // (intersect_point (p) - pointOnPlane (p)).dot(normal) = 0
     // intersect_point = (orig + dir * t)
     // => t = (intersect point - orig).dot(normal) / dir.dot(normal)
-    const float denom = dir.dotProduct(normal);
+    const double denom = dir.dotProduct(normal);
     if (denom < -1e-6) {
-      Vec3f p0l0 = pointOnPlane - orig;
+      Vec3d p0l0 = pointOnPlane - orig;
       t = p0l0.dotProduct(normal) / denom;
-
       return (t > 0);
     }
 
     return false;
   }
 
-  void getSurfaceData([[maybe_unused]] const Vec3f &point, Vec3f &norm,
-                      Vec3f &col) const {
+  void getSurfaceData([[maybe_unused]] const Vec3d &point, Vec3d &norm,
+                      Vec3d &col) const {
     norm = normal;
-    const Vec3f tex = point.crossProduct(normal);
+    const Vec3d tex = point.crossProduct(normal);
     col = color * texture.getVal(tex.x, tex.z, 2);
   }
 
   const TEX_T texture{};
-  const Vec3f normal;
-  const Vec3f pointOnPlane;
+  const Vec3d normal;
+  const Vec3d pointOnPlane;
 };
 
 // Implicit torus surface
 class Torus : public ImplicitShape {
 public:
-  Torus(const Vec3f &_center, const float &_r0, const float &_r1)
+  Torus(const Vec3d &_center, const double &_r0, const double &_r1)
       : center(_center), r0(_r0), r1(_r1) {}
-  float getDistance(const Vec3f &from) const {
-    Vec3f pointObjectSpace = from - center;
+  double getDistance(const Vec3d &from) const {
+    Vec3d pointObjectSpace = from - center;
     // reduce 3D point to 2D point
-    const float tmpx = sqrtf(pointObjectSpace.x * pointObjectSpace.x +
+    const double tmpx = sqrtf(pointObjectSpace.x * pointObjectSpace.x +
                              pointObjectSpace.z * pointObjectSpace.z) -
                        r0;
-    const float tmpy = pointObjectSpace.y;
+    const double tmpy = pointObjectSpace.y;
 
     // distance to cicle
     return sqrtf(tmpx * tmpx + tmpy * tmpy) - r1;
   }
-  bool intersect(const Vec3f &orig, const Vec3f &dir, float &t) const {
-    float distance;
+  bool intersect(const Vec3d &orig, const Vec3d &dir, double &t) const {
+    double distance;
     for (t = 0; t < MAX_DISTANCE; t += distance) {
-      Vec3f point = orig + t * dir;
+      Vec3d point = orig + t * dir;
       distance = getDistance(point);
 
       if (distance <= INTERSECT_TH * t) {
@@ -128,45 +128,45 @@ public:
   // Method to compute the surface data such as normal and texture coordinates
   // at the intersection point. See method implementation in children class for
   // details
-  void getSurfaceData([[maybe_unused]] const Vec3f &point, Vec3f &norm,
-                      Vec3f &col) const {
-    constexpr float delta = 10e-5;
-    const float base = getDistance(point);
-    norm = Vec3f(getDistance(point + Vec3f(delta, 0, 0)) - base,
-                 getDistance(point + Vec3f(0, delta, 0)) - base,
-                 getDistance(point + Vec3f(0, 0, delta)) - base)
+  void getSurfaceData([[maybe_unused]] const Vec3d &point, Vec3d &norm,
+                      Vec3d &col) const {
+    constexpr double delta = 10e-5;
+    const double base = getDistance(point);
+    norm = Vec3d(getDistance(point + Vec3d(delta, 0, 0)) - base,
+                 getDistance(point + Vec3d(0, delta, 0)) - base,
+                 getDistance(point + Vec3d(0, 0, delta)) - base)
                .normalize();
     col = color;
   }
-  const Vec3f center;
-  const float r0, r1;
+  const Vec3d center;
+  const double r0, r1;
 };
 
 // Implicit cube surface
 class Cube : public ImplicitShape {
 public:
-  Cube(const Vec3f &_corner) : corner(_corner) {}
-  inline float getDistance(const Vec3f &point) const {
-    constexpr float scale = 1;
+  Cube(const Vec3d &_corner) : corner(_corner) {}
+  inline double getDistance(const Vec3d &point) const {
+    constexpr double scale = 1;
 
-    Vec3f pointObjectSpace = point * 1.f / scale;
+    Vec3d pointObjectSpace = point * 1.f / scale;
     pointObjectSpace.x = std::fabs(pointObjectSpace.x);
     pointObjectSpace.y = std::fabs(pointObjectSpace.y);
     pointObjectSpace.z = std::fabs(pointObjectSpace.z);
 
     // now compute the distance from the point to the surface of the object
-    Vec3f d = pointObjectSpace - corner;
+    Vec3d d = pointObjectSpace - corner;
 
-    float dmaxLen = sqrt(d.x * d.x * (d.x > 0) + d.y * d.y * (d.y > 0) +
+    double dmaxLen = sqrt(d.x * d.x * (d.x > 0) + d.y * d.y * (d.y > 0) +
                          d.z * d.z * (d.z > 0));
 
     // don't forget to apply the scale back
     return scale * (std::min(std::max(d.x, std::max(d.y, d.z)), 0.f) + dmaxLen);
   }
-  bool intersect(const Vec3f &orig, const Vec3f &dir, float &t) const {
-    float distance;
+  bool intersect(const Vec3d &orig, const Vec3d &dir, double &t) const {
+    double distance;
     for (t = 0; t < MAX_DISTANCE; t += distance) {
-      Vec3f point = orig + t * dir;
+      Vec3d point = orig + t * dir;
       distance = getDistance(point);
 
       if (distance <= INTERSECT_TH * t) {
@@ -179,44 +179,44 @@ public:
   // Method to compute the surface data such as normal and texture coordinates
   // at the intersection point. See method implementation in children class for
   // details
-  void getSurfaceData([[maybe_unused]] const Vec3f &point, Vec3f &norm,
-                      Vec3f &col) const {
-    constexpr float delta = 10e-5;
-    const float base = getDistance(point);
-    norm = Vec3f(getDistance(point + Vec3f(delta, 0, 0)) - base,
-                 getDistance(point + Vec3f(0, delta, 0)) - base,
-                 getDistance(point + Vec3f(0, 0, delta)) - base)
+  void getSurfaceData([[maybe_unused]] const Vec3d &point, Vec3d &norm,
+                      Vec3d &col) const {
+    constexpr double delta = 10e-5;
+    const double base = getDistance(point);
+    norm = Vec3d(getDistance(point + Vec3d(delta, 0, 0)) - base,
+                 getDistance(point + Vec3d(0, delta, 0)) - base,
+                 getDistance(point + Vec3d(0, 0, delta)) - base)
                .normalize();
     col = color;
   }
-  const Vec3f corner{};
+  const Vec3d corner{};
 };
 
 struct unionFunc {
-  float operator()(float a, float b) const { return std::min(a, b); }
+  double operator()(double a, double b) const { return std::min(a, b); }
 };
 
 struct subtractFunc {
-  float operator()(float a, float b) const { return std::max(-a, b); }
+  double operator()(double a, double b) const { return std::max(-a, b); }
 };
 
 struct intersectionFunc {
-  float operator()(float a, float b) const { return std::max(a, b); }
+  double operator()(double a, double b) const { return std::max(a, b); }
 };
 
 struct blendFunc {
-  blendFunc(const float &_k) : k(_k) {}
-  float operator()(float a, float b) const {
-    float res = exp(-k * a) + exp(-k * b);
+  blendFunc(const double &_k) : k(_k) {}
+  double operator()(double a, double b) const {
+    double res = exp(-k * a) + exp(-k * b);
     return -log(std::max(0.0001f, res)) / k;
   }
-  float k;
+  double k;
 };
 
 struct mixFunc {
-  mixFunc(const float &_t) : t(_t) {}
-  float operator()(float a, float b) const { return a * (1 - t) + b * t; }
-  float t;
+  mixFunc(const double &_t) : t(_t) {}
+  double operator()(double a, double b) const { return a * (1 - t) + b * t; }
+  double t;
 };
 
 // Combining implict shapes using CSG
@@ -225,7 +225,7 @@ class CSG : public ImplicitShape {
 public:
   CSG(const S1 &s1, const S2 &s2, Args &&... args)
       : op(std::forward<Args>(args)...), shape1(s1), shape2(s2) {}
-  float getDistance(const Vec3f &from) const {
+  double getDistance(const Vec3d &from) const {
     return op(shape1.getDistance(from), shape2.getDistance(from));
   }
   Op op;
@@ -236,30 +236,30 @@ public:
 // Blobbies
 class SoftObject : public ImplicitShape {
   struct Blob {
-    float R; // radius
-    Vec3f c; // blob center
+    double R; // radius
+    Vec3d c; // blob center
   };
 
 public:
   SoftObject() {
 #if 0
-        blobbies.push_back({2.0, Vec3f(-1, 0, 0)});
-        blobbies.push_back({1.5, Vec3f( 1, 0, 0)});
+        blobbies.push_back({2.0, Vec3d(-1, 0, 0)});
+        blobbies.push_back({1.5, Vec3d( 1, 0, 0)});
 #else
     for (size_t i = 0; i < 20; ++i) {
-      float radius = (0.3 + drand48() * 1.3);
-      Vec3f c((0.5 - drand48()) * 3, (0.5 - drand48()) * 3,
+      double radius = (0.3 + drand48() * 1.3);
+      Vec3d c((0.5 - drand48()) * 3, (0.5 - drand48()) * 3,
               (0.5 - drand48()) * 3);
       blobbies.push_back({radius, c});
     }
 #endif
   }
-  float getDistance(const Vec3f &from) const {
-    float sumDensity = 0;
-    float sumRi = 0;
-    float minDistance = std::numeric_limits<float>::max();
+  double getDistance(const Vec3d &from) const {
+    double sumDensity = 0;
+    double sumRi = 0;
+    double minDistance = std::numeric_limits<double>::max();
     for (const auto &blob : blobbies) {
-      float r = (blob.c - from).length();
+      double r = (blob.c - from).length();
       if (r <= blob.R) {
         // this can be factored for speed if you want
         sumDensity += 2 * (r * r * r) / (blob.R * blob.R * blob.R) -
@@ -271,7 +271,7 @@ public:
 
     return std::max(minDistance, (magic - sumDensity) / (3 / 2.f * sumRi));
   }
-  float magic{0.2};
+  double magic{0.2};
   std::vector<Blob> blobbies;
 };
 
@@ -282,6 +282,6 @@ template <typename S1, typename S2> using Subtract = CSG<subtractFunc, S1, S2>;
 template <typename S1, typename S2>
 using Intersect = CSG<intersectionFunc, S1, S2>;
 
-template <typename S1, typename S2> using Blend = CSG<blendFunc, S1, S2, float>;
+template <typename S1, typename S2> using Blend = CSG<blendFunc, S1, S2, double>;
 
-template <typename S1, typename S2> using Mix = CSG<mixFunc, S1, S2, float>;
+template <typename S1, typename S2> using Mix = CSG<mixFunc, S1, S2, double>;
